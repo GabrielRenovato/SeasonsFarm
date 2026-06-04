6125ea1ee5f479ed448c758d1f748bdc6648a9ae
Renovato <gabrielrenovatodasilva@gmail.com>
2026-06-04 10:21:00 -0300

    feat: refatoring

--- test_sit_frames.py
+++
@@ -1,9 +0,0 @@
-with open('c:/Users/ofici/OneDrive/Documentos/farm-gaming/entities/player/player.tscn', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-for anim in ['sit_down', 'sit_up', 'sit_right', 'sit_left']:
-    idx = text.find('resource_name = "' + anim + '"')
-    frame_idx = text.find('NodePath("Body:frame")', idx)
-    keys_idx = text.find('"values": [', frame_idx)
-    end_idx = text.find(']', keys_idx)
-    print(anim, text[keys_idx:end_idx+1])
