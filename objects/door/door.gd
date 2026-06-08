700e6e1df49b95546a78ea049ec2b2c848cb5840
Renovato <gabrielrenovatodasilva@gmail.com>
2026-06-07 22:53:41 -0300

    chore: add door debug visual and increase size

--- objects/door/door.gd
+++ objects/door/door.gd
@@ -12,8 +12,10 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
+	print(" DOOR TRIGGERED BY: ", body.name, " Groups: ", body.get_groups())
 	# Se o corpo for o Player e tivermos uma cena configurada (If body is Player and we have a target scene)
 	if body.is_in_group("player") and target_scene_path != "":
+		print(" Transitioning to: ", target_scene_path)
 		# Chama o SceneManager global para fazer a transição (Call global SceneManager to transition)
 		# O autoload SceneManager já deve estar configurado no project.godot
 		if Engine.has_singleton("SceneManager"):
